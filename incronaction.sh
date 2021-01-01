c40740d7f01d89861f7efd8f8216df0615b4bfb6
Tarun Sharma <starun.1998@gmail.com>
2021-01-01 19:14:56 +0530

    incron script to fill template in acpp file

---
+++ incronaction.sh
@@ -0,0 +1,13 @@
+#! bin/bash 
+
+# $1 << ~/dsa/cp/template.cpp 
+
+# touch myfile.txt 
+FILENAME=$1
+EXTENSION=${FILENAME#*.}
+echo "$EXTENSION"
+if [ "$EXTENSION" = "cpp" ] ; then 
+       cp /home/dunno/dsa/cp/template.cpp /home/dunno/dsa/cp/$1
+#	echo "TARUN"
+fi
+
