952429c33e3c3f63571f8b2ea30d0f646448c2d0
Tarun Sharma <starun.1998@gmail.com>
2021-01-09 18:50:58 +0530

    font script

--- incronaction.sh
+++ incronaction.sh
@@ -5,9 +5,11 @@
 # touch myfile.txt 
 FILENAME=$1
 EXTENSION=${FILENAME#*.}
-echo "$EXTENSION"
-if [ "$EXTENSION" = "cpp" ] ; then 
+echo "$EXTENSION" 
+if [ "$EXTENSION" != "cpp" ] ; then 
+	exit 1 
+elif [ "$EXTENSION" = "cpp" ] ; then 
        cp /home/dunno/dsa/cp/template.cpp /home/dunno/dsa/cp/$1
-#	echo "TARUN"
+	echo "TARUN"
 fi
 
